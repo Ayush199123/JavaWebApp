de7bc81d3bc239483c23d94a6ea915328ec32689
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2022-12-01 12:37:16 +0530

    ALL Files Are Updated

---
+++ src/main/java/com/mycompany/javawebprpject/WelcomeServlet.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.javawebprpject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//import com.mongodb.MongoClient;
+//import com.mongodb.MongoClientURI;
+//import com.mongodb.client.MongoCollection;
+//import com.mongodb.client.MongoCursor;
+//import com.mongodb.client.MongoDatabase;
+//import org.bson.Document;
+//import static com.mongodb.client.model.Filters.eq;
+//import com.mongodb.client.model.Filters.*;
+//import javax.servlet.RequestDispatcher;
+//import org.bson.Document;
+//import org.bson.conversions.Bson;
+
+/**
+ *
+ * @author Ayush
+ */
+public class WelcomeServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet WelcomeServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet WelcomeServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html;charset=UTF-8");
+        out.println("<html><head>");
+        out.println("<title>Ser</title></head>");
+        String uname = request.getParameter("txtId");
+        String upass = request.getParameter("txtPass");
+        RequestDispatcher rd = request.getRequestDispatcher("WelcomeServlet");
+        rd.forward(request, response);
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+}
